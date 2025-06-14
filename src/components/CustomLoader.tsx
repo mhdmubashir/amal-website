78434225bc1e904877ff0cf1118ff38c09eb5b92
Muhammed Mubashir <119051377+mhdmubashir@users.noreply.github.com>
2025-06-14 23:33:32 +0530

    upd

--- src/components/CustomLoader.tsx
+++ src/components/CustomLoader.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
+const fallbackImage = "/images/logo.webp";
 
 const CustomLoader = () => {
   const [dots, setDots] = useState(".");
@@ -7,14 +8,14 @@ const CustomLoader = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setDots((prev) => (prev.length >= 3 ? "." : prev + "."));
-    }, 500);
+    }, 100000);
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen bg-white">
       <div className="animate-pulse">
-        <Image src="/image/logo.webp" alt="Logo" width={100} height={100} />
+        <Image src={fallbackImage} alt="Logo" width={100} height={100} />
       </div>
       <div className="mt-4 text-lg font-medium text-gray-700">
         Loading{dots}
