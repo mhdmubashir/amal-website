a8dd6d0113a9df2834bca291ef31c9b97173bd97
Muhammed Mubashir T <msfiuml2@gmail.com>
2025-06-20 05:07:16 +0530

    api event chng

--- src/services/event/event_service.ts
+++ src/services/event/event_service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { apiBase } from "../const_service";
 import { EventData } from "../../types";
+import { apiBase } from "../const_service";
 
 const apiClient = axios.create({
   baseURL: apiBase,
@@ -8,7 +8,7 @@ const apiClient = axios.create({
 
 export const eventService = {
   getEvents: async (params?: { limit?: number; page?: number }): Promise<EventData[]> => {
-    const response = await apiClient.get("/events", { params });
+    const response = await apiClient.get("/events/public", { params });
     return response.data['data'];
   },
 };
